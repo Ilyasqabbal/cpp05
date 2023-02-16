4a502819186cdb847e7600f8d2290ec5f048aa0b
Ilyas qabbal <ilyas.qabbal.2020@usms.ac.ma>
2023-02-16 06:10:28 +0100

    commit

--- ex03/Intern.cpp
+++ ex03/Intern.cpp
@@ -6,7 +6,7 @@
 /*   By: iqabbal <iqabbal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 00:27:16 by iqabbal           #+#    #+#             */
-/*   Updated: 2023/02/16 05:45:03 by iqabbal          ###   ########.fr       */
+/*   Updated: 2023/02/16 06:07:28 by iqabbal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,22 +52,27 @@ AForm *Intern::makeForm(std::string NameForm,std::string Target)
 {
     Intern intern;
     std::string Forms[3] = {"ShrubberyCreationForm","RobotomyRequestForm","PresidentialPardonForm"};
-    if(Forms[0] == NameForm)
+    int i = -1;
+    while(++i < 3 && NameForm != Forms[i])
     {
-        std::cout << "Intern creates" ;
-        return(intern.createShrubberyCreationForm(Target));
+        
     }
-    else if(Forms[1] == NameForm)
+    switch (i)
     {
-        std::cout << "Intern creates" ;
-        return(intern.createRobotomyRequestForm(Target));
+        case 0 :
+            std::cout << "Intern creates" ;
+            return(intern.createShrubberyCreationForm(Target));
+            break;
+        case 1 :
+            std::cout << "Intern creates" ;
+            return(intern.createRobotomyRequestForm(Target));
+            break;
+        case 2 :
+            std::cout << "Intern creates" ;
+            return(intern.createPresidentialPardonForm(Target));
+            break;
+        default :
+            std::cout << "enter a valid form name" << std::endl;
     }
-    else if(Forms[2] == NameForm)
-    {
-        std::cout << "Intern creates" ;
-        return(intern.createPresidentialPardonForm(Target));
-    }
-    else
-        std::cout << "enter a valid name form" << std::endl;
     return NULL;
 }
--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: iqabbal <iqabbal@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 13:19:27 by iqabbal           #+#    #+#             */
-/*   Updated: 2023/02/16 05:30:16 by iqabbal          ###   ########.fr       */
+/*   Updated: 2023/02/16 06:09:30 by iqabbal          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,41 @@ int main()
 {
     try
 	{
-		Bureaucrat worker("worker", 149);
+		Bureaucrat worker("worker", 1);
+		Intern someRandomIntern;
+		AForm* rrf;
+		rrf = someRandomIntern.makeForm("ShrubberyCreationForm", "Bender");
+		rrf->execute(worker);
+
+		worker.signForm(*rrf);
+		worker.executeForm(*rrf);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+    try
+	{
+		Bureaucrat worker("worker", 150);
 		Intern someRandomIntern;
 		AForm* rrf;
 		rrf = someRandomIntern.makeForm("RobotomyRequestForm", "Bender");
 		rrf->execute(worker);
+		worker.signForm(*rrf);
+		worker.executeForm(*rrf);
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 
+    try
+	{
+		Bureaucrat worker("worker", 33);
+		Intern someRandomIntern;
+		AForm* rrf;
+		rrf = someRandomIntern.makeForm("PresidentialPardonForm", "Bender");
+		rrf->execute(worker);
 		worker.signForm(*rrf);
 		worker.executeForm(*rrf);
 	}
